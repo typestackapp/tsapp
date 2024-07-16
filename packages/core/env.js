3aca16e36f057655a4655c5f533a783710b2f8f1
Juugis <bruvers.jurgis@gmail.com>
2024-07-16 10:28:42 +0300

    change ports form int to string

--- packages/core/env.js
+++ packages/core/env.js
@@ -4,7 +4,7 @@ exports.codeserver = exports.wireguard = exports.sftp = exports.rabbitmq = expor
 const env_1 = require("@typestackapp/cli/common/env");
 exports.tsapp = new env_1.ENV({
     TSAPP_ENV_TYPE: env_1.zod.string(),
-    TSAPP_PORT: env_1.zod.coerce.number().int(),
+    TSAPP_PORT: env_1.zod.string(),
     TSAPP_IP: env_1.zod.string().ip(),
     TSAPP_SUBNET: env_1.zod.string(),
     TSAPP_TIME_ZONE: env_1.zod.string(),
@@ -13,12 +13,12 @@ exports.tsapp = new env_1.ENV({
     TSAPP_INIT_PSW: env_1.zod.string(),
     TSAPP_INIT_EMAIL: env_1.zod.string().email(),
     TSAPP_RCS: env_1.zod.string().default("").optional(),
-    TSAPP_NEXT_PORT: env_1.zod.coerce.number().int().default(8010).optional(),
-    TSAPP_API_PORT: env_1.zod.coerce.number().int().default(8011).optional(),
-    TSAPP_GRAPHQL_PORT: env_1.zod.coerce.number().int().default(8013).optional(),
+    TSAPP_NEXT_PORT: env_1.zod.string().default("8010").optional(),
+    TSAPP_API_PORT: env_1.zod.string().default("8011").optional(),
+    TSAPP_GRAPHQL_PORT: env_1.zod.string().default("8013").optional(),
 }, {
     TSAPP_ENV_TYPE: "dev",
-    TSAPP_PORT: 8000,
+    TSAPP_PORT: "8000",
     TSAPP_IP: "10.44.44.44",
     TSAPP_SUBNET: "10.44.44.0/24",
     TSAPP_TIME_ZONE: "UTC",
@@ -27,9 +27,9 @@ exports.tsapp = new env_1.ENV({
     TSAPP_INIT_PSW: "root-psw",
     TSAPP_INIT_EMAIL: "test@test.com",
     TSAPP_RCS: "",
-    TSAPP_NEXT_PORT: 8010,
-    TSAPP_API_PORT: 8011,
-    TSAPP_GRAPHQL_PORT: 8013
+    TSAPP_NEXT_PORT: "8010",
+    TSAPP_API_PORT: "8011",
+    TSAPP_GRAPHQL_PORT: "8013"
 }, {
     default: true,
 });
@@ -37,18 +37,18 @@ exports.haproxy = new env_1.ENV({
     HAPROXY_IP: env_1.zod.string().ip(),
     HAPROXY_PORT: env_1.zod.string(),
     HAPROXY_STATS_AUTH: env_1.zod.string(),
-    HAPROXY_TSAPP_PORT: env_1.zod.coerce.number().int(),
+    HAPROXY_TSAPP_PORT: env_1.zod.string(),
 }, {
     HAPROXY_IP: "10.44.44.41",
     HAPROXY_PORT: '["7443:443", "7444:9003", "7445:8404"]',
     HAPROXY_STATS_AUTH: "haproxy:root-psw",
-    HAPROXY_TSAPP_PORT: 7443,
+    HAPROXY_TSAPP_PORT: "7443",
 }, {
     default: true,
 });
 exports.certbot = new env_1.ENV({
     CERTBOT_IP: env_1.zod.string().ip(),
-    CERTBOT_PORT: env_1.zod.coerce.number().int(),
+    CERTBOT_PORT: env_1.zod.string(),
     CERTBOT_SELFSIGNED: env_1.zod.coerce.boolean().default(false).optional(),
     CERTBOT_INIT: env_1.zod.coerce.boolean().default(false).optional(),
     CERTBOT_RESTART_TIME: env_1.zod.string().default("12h").optional(), // [12s, 12h]
@@ -56,7 +56,7 @@ exports.certbot = new env_1.ENV({
     CERTBOT_EMAIL: env_1.zod.string().email(),
 }, {
     CERTBOT_IP: "10.44.44.40",
-    CERTBOT_PORT: 80,
+    CERTBOT_PORT: "80",
     CERTBOT_SELFSIGNED: true,
     CERTBOT_INIT: false,
     CERTBOT_RESTART_TIME: "12h",
@@ -64,7 +64,7 @@ exports.certbot = new env_1.ENV({
     CERTBOT_EMAIL: "test@test.com"
 });
 exports.mongo = new env_1.ENV({
-    MONGO_PORT: env_1.zod.coerce.number().int(),
+    MONGO_PORT: env_1.zod.string(),
     MONGO_IP: env_1.zod.string().ip(),
     MONGO_INITDB_NAME: env_1.zod.string(),
     MONGO_INITDB_ROOT_USERNAME: env_1.zod.string(),
@@ -73,7 +73,7 @@ exports.mongo = new env_1.ENV({
     MONGO_DB_PATH: env_1.zod.string(),
     MONGO_BIND_IP: env_1.zod.string().ip(),
 }, {
-    MONGO_PORT: 27017,
+    MONGO_PORT: "27017",
     MONGO_IP: "10.44.44.43",
     MONGO_INITDB_NAME: "tsapp",
     MONGO_INITDB_ROOT_USERNAME: "root",
@@ -83,25 +83,25 @@ exports.mongo = new env_1.ENV({
     MONGO_BIND_IP: "0.0.0.0"
 });
 exports.rabbitmq = new env_1.ENV({
-    RABBITMQ_PORT: env_1.zod.coerce.number().int(),
+    RABBITMQ_PORT: env_1.zod.string(),
     RABBITMQ_IP: env_1.zod.string().ip(),
     RABBITMQ_DEFAULT_USER: env_1.zod.string(),
     RABBITMQ_DEFAULT_PASS: env_1.zod.string(),
 }, {
-    RABBITMQ_PORT: 7444,
+    RABBITMQ_PORT: "7444",
     RABBITMQ_IP: "10.44.44.42",
     RABBITMQ_DEFAULT_USER: "root",
     RABBITMQ_DEFAULT_PASS: "root-psw",
 });
 exports.sftp = new env_1.ENV({
-    SFTP_PORT: env_1.zod.coerce.number().int().default(22),
+    SFTP_PORT: env_1.zod.string().default("22"),
     SFTP_IP: env_1.zod.string().ip().default("10.44.44.46"),
 }, {
-    SFTP_PORT: 22,
+    SFTP_PORT: "22",
     SFTP_IP: "10.44.44.46"
 });
 exports.wireguard = new env_1.ENV({
-    WIREGUARD_PORT: env_1.zod.coerce.number().int().default(51820),
+    WIREGUARD_PORT: env_1.zod.string().default("51820"),
     WIREGUARD_IP: env_1.zod.string().ip().default("10.44.44.45"),
     WIREGUARD_SERVERURL: env_1.zod.string().default("auto"),
     WIREGUARD_PEERS: env_1.zod.coerce.number().int().default(50),
@@ -109,7 +109,7 @@ exports.wireguard = new env_1.ENV({
     WIREGUARD_ALLOWEDIPS: env_1.zod.string().default("10.44.44.0/24"),
     WIREGUARD_INTERNAL_SUBNET: env_1.zod.string().ip().default("10.10.1.0")
 }, {
-    WIREGUARD_PORT: 51820,
+    WIREGUARD_PORT: "51820",
     WIREGUARD_IP: "10.44.44.45",
     WIREGUARD_SERVERURL: "auto",
     WIREGUARD_PEERS: 50,
@@ -118,7 +118,7 @@ exports.wireguard = new env_1.ENV({
     WIREGUARD_INTERNAL_SUBNET: "10.10.1.0"
 });
 exports.codeserver = new env_1.ENV({
-    CODESERVER_PORT: env_1.zod.coerce.number().int().default(8015),
+    CODESERVER_PORT: env_1.zod.string().default("8015"),
     CODESERVER_IP: env_1.zod.string().ip().default("10.44.44.47"),
     CODESERVER_PASSWORD: env_1.zod.string().default("root-psw"),
     CODESERVER_HASHED_PASSWORD: env_1.zod.string().default(""),
@@ -127,7 +127,7 @@ exports.codeserver = new env_1.ENV({
     CODESERVER_PROXY_DOMAIN: env_1.zod.string().default("codeserver"),
     CODESERVER_DEFAULT_WORKSPACE: env_1.zod.string().default("/tsapp/"),
 }, {
-    CODESERVER_PORT: 8015,
+    CODESERVER_PORT: "8015",
     CODESERVER_IP: "10.44.44.47",
     CODESERVER_PASSWORD: "root-psw",
     CODESERVER_HASHED_PASSWORD: "",
--- packages/core/env.ts
+++ packages/core/env.ts
@@ -3,7 +3,7 @@ import { ENV, zod } from "@typestackapp/cli/common/env"
 export const tsapp = new ENV(
     {
         TSAPP_ENV_TYPE: zod.string(),
-        TSAPP_PORT: zod.coerce.number().int(),
+        TSAPP_PORT: zod.string(),
         TSAPP_IP: zod.string().ip(),
         TSAPP_SUBNET: zod.string(),
         TSAPP_TIME_ZONE: zod.string(),
@@ -12,13 +12,13 @@ export const tsapp = new ENV(
         TSAPP_INIT_PSW: zod.string(),
         TSAPP_INIT_EMAIL: zod.string().email(),
         TSAPP_RCS: zod.string().default("").optional(),
-        TSAPP_NEXT_PORT: zod.coerce.number().int().default(8010).optional(),
-        TSAPP_API_PORT: zod.coerce.number().int().default(8011).optional(),
-        TSAPP_GRAPHQL_PORT: zod.coerce.number().int().default(8013).optional(),
+        TSAPP_NEXT_PORT: zod.string().default("8010").optional(),
+        TSAPP_API_PORT: zod.string().default("8011").optional(),
+        TSAPP_GRAPHQL_PORT: zod.string().default("8013").optional(),
     },
     {
         TSAPP_ENV_TYPE: "dev",
-        TSAPP_PORT: 8000,
+        TSAPP_PORT: "8000",
         TSAPP_IP: "10.44.44.44",
         TSAPP_SUBNET: "10.44.44.0/24",
         TSAPP_TIME_ZONE: "UTC",
@@ -27,9 +27,9 @@ export const tsapp = new ENV(
         TSAPP_INIT_PSW: "root-psw",
         TSAPP_INIT_EMAIL: "test@test.com",
         TSAPP_RCS: "",
-        TSAPP_NEXT_PORT: 8010,
-        TSAPP_API_PORT: 8011,
-        TSAPP_GRAPHQL_PORT: 8013
+        TSAPP_NEXT_PORT: "8010",
+        TSAPP_API_PORT: "8011",
+        TSAPP_GRAPHQL_PORT: "8013"
     },
     {
         default: true,
@@ -41,13 +41,13 @@ export const haproxy = new ENV(
         HAPROXY_IP: zod.string().ip(),
         HAPROXY_PORT: zod.string(),
         HAPROXY_STATS_AUTH: zod.string(),
-        HAPROXY_TSAPP_PORT: zod.coerce.number().int(),
+        HAPROXY_TSAPP_PORT: zod.string(),
     },
     {
         HAPROXY_IP: "10.44.44.41",
         HAPROXY_PORT: '["7443:443", "7444:9003", "7445:8404"]',
         HAPROXY_STATS_AUTH: "haproxy:root-psw",
-        HAPROXY_TSAPP_PORT: 7443,
+        HAPROXY_TSAPP_PORT: "7443",
     },
     {
         default: true,
@@ -57,7 +57,7 @@ export const haproxy = new ENV(
 export const certbot = new ENV(
     {
         CERTBOT_IP: zod.string().ip(),
-        CERTBOT_PORT: zod.coerce.number().int(),
+        CERTBOT_PORT: zod.string(),
         CERTBOT_SELFSIGNED: zod.coerce.boolean().default(false).optional(),
         CERTBOT_INIT: zod.coerce.boolean().default(false).optional(),
         CERTBOT_RESTART_TIME: zod.string().default("12h").optional(), // [12s, 12h]
@@ -66,7 +66,7 @@ export const certbot = new ENV(
     },
     {
         CERTBOT_IP: "10.44.44.40",
-        CERTBOT_PORT: 80,
+        CERTBOT_PORT: "80",
         CERTBOT_SELFSIGNED: true,
         CERTBOT_INIT: false,
         CERTBOT_RESTART_TIME: "12h",
@@ -77,7 +77,7 @@ export const certbot = new ENV(
 
 export const mongo = new ENV(
     {
-        MONGO_PORT: zod.coerce.number().int(),
+        MONGO_PORT: zod.string(),
         MONGO_IP: zod.string().ip(),
         MONGO_INITDB_NAME: zod.string(),
         MONGO_INITDB_ROOT_USERNAME: zod.string(),
@@ -87,7 +87,7 @@ export const mongo = new ENV(
         MONGO_BIND_IP: zod.string().ip(),
     },
     {
-        MONGO_PORT: 27017,
+        MONGO_PORT: "27017",
         MONGO_IP: "10.44.44.43",
         MONGO_INITDB_NAME: "tsapp",
         MONGO_INITDB_ROOT_USERNAME: "root",
@@ -100,13 +100,13 @@ export const mongo = new ENV(
 
 export const rabbitmq = new ENV(
     {
-        RABBITMQ_PORT: zod.coerce.number().int(),
+        RABBITMQ_PORT: zod.string(),
         RABBITMQ_IP: zod.string().ip(),
         RABBITMQ_DEFAULT_USER: zod.string(),
         RABBITMQ_DEFAULT_PASS: zod.string(),
     }, 
     {
-        RABBITMQ_PORT: 7444,
+        RABBITMQ_PORT: "7444",
         RABBITMQ_IP: "10.44.44.42",
         RABBITMQ_DEFAULT_USER: "root",
         RABBITMQ_DEFAULT_PASS: "root-psw",
@@ -115,18 +115,18 @@ export const rabbitmq = new ENV(
 
 export const sftp = new ENV(
     {
-        SFTP_PORT: zod.coerce.number().int().default(22),
+        SFTP_PORT: zod.string().default("22"),
         SFTP_IP: zod.string().ip().default("10.44.44.46"),
     },
     {
-        SFTP_PORT: 22,
+        SFTP_PORT: "22",
         SFTP_IP: "10.44.44.46"
     }
 )
 
 export const wireguard = new ENV(
     {
-        WIREGUARD_PORT: zod.coerce.number().int().default(51820),
+        WIREGUARD_PORT: zod.string().default("51820"),
         WIREGUARD_IP: zod.string().ip().default("10.44.44.45"),
         WIREGUARD_SERVERURL: zod.string().default("auto"),
         WIREGUARD_PEERS: zod.coerce.number().int().default(50),
@@ -135,7 +135,7 @@ export const wireguard = new ENV(
         WIREGUARD_INTERNAL_SUBNET: zod.string().ip().default("10.10.1.0")
     },
     {
-        WIREGUARD_PORT: 51820,
+        WIREGUARD_PORT: "51820",
         WIREGUARD_IP: "10.44.44.45",
         WIREGUARD_SERVERURL: "auto",
         WIREGUARD_PEERS: 50,
@@ -147,7 +147,7 @@ export const wireguard = new ENV(
 
 export const codeserver = new ENV(
     {
-        CODESERVER_PORT: zod.coerce.number().int().default(8015),
+        CODESERVER_PORT: zod.string().default("8015"),
         CODESERVER_IP: zod.string().ip().default("10.44.44.47"),
         CODESERVER_PASSWORD: zod.string().default("root-psw"),
         CODESERVER_HASHED_PASSWORD: zod.string().default(""),
@@ -157,7 +157,7 @@ export const codeserver = new ENV(
         CODESERVER_DEFAULT_WORKSPACE: zod.string().default("/tsapp/"),
     },
     {
-        CODESERVER_PORT: 8015,
+        CODESERVER_PORT: "8015",
         CODESERVER_IP: "10.44.44.47",
         CODESERVER_PASSWORD: "root-psw",
         CODESERVER_HASHED_PASSWORD: "",
