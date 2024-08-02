192e6b15ea66e995a0d0c4d4390745bc59f39822
Juugis <bruvers.jurgis@gmail.com>
2024-08-02 15:13:46 +0300

    core clenup next.js config

--- packages/core/codegen/next/next.config.ts
+++ packages/core/codegen/next/next.config.ts
@@ -11,34 +11,11 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-        // follow symlink
-        // config.resolve.symlinks = true
-
-        // // hash all files
         config.watchOptions = {
             //followSymlinks: true,
             aggregateTimeout: 200,
             poll: 1000,
         }
-
-        // config.cache = false
-
-        // config.devServer = {
-        //     hot: true
-        // }
-
-        // use bable to transpile typescript
-        // config.module.rules.push({
-        //     test: /.(ts|tsx)$/,
-        //     exclude: /node_modules/,
-        //     use: [{
-        //         loader: "babel-loader",
-        //         options: {
-        //             presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-        //         }
-        //     }],
-        // })
-
         return config
     },
     async rewrites() {
