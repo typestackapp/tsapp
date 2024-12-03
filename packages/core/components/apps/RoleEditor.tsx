2af739558ba80b46122ac3267929a9db21946fe6
Jurgis Bruvers <juugis777@inbox.lv>
2024-12-04 00:13:56 +0200

    re

--- packages/core/components/apps/RoleEditor.tsx
+++ packages/core/components/apps/RoleEditor.tsx
@@ -6,10 +6,8 @@ import { getRoleManagerData, useQuery, client } from '@typestackapp/core/compone
 import { AccessValidator } from '@typestackapp/core/models/user/access/util'
 import { ValuesType } from 'utility-types'
 
-
-type AccessStructure = {
-  [role: string]: client.GetRoleManagerDataQuery["getAllAccessConfigs"]
-}
+type AccessConfig = ValuesType<NonNullable<client.GetRoleManagerDataQuery["getAllAccessConfigs"]>>
+type Scope = ValuesType<NonNullable<client.GetRoleManagerDataQuery["getAllAccessConfigs"]>>
 
 const permissions: client.PermissionType[] = [
     client.PermissionType.Read, 
@@ -18,69 +16,59 @@ const permissions: client.PermissionType[] = [
     client.PermissionType.Update
 ]
 const getAlias = (pack: string, data: client.GetRoleManagerDataQuery) => data?.getAllPackageConfigs.find(config => config.pack === pack)?.alias
-const getRole = (roleId: string | undefined, data: client.GetRoleManagerDataQuery) => data?.getAllRoles?.find(role => role._id === roleId)
+const getRole = (roleID: string | undefined, data: client.GetRoleManagerDataQuery) => data?.getAllRoles?.find(role => role._id === roleID)
 
 export default function RoleEditor() {
     const { tsappClient, session } = React.useContext(context)
     const query = useQuery(getRoleManagerData)
     const [data, setData] = React.useState<client.GetRoleManagerDataQuery | undefined>(query.data)
-    const [selectedRole, setSelectedRole] = React.useState<string>()
+    const [roleID, setRoleID] = React.useState<string>()
+    const [scope, setScope] = React.useState<Scope>()
 
     React.useEffect(() => {
         setData(query.data)
         if (query.data?.getAllRoles) {
-            setSelectedRole(query.data.getAllRoles[0]._id)
+            setRoleID(query.data.getAllRoles[0]._id)
+            setScope(query.data.getAllAccessConfigs[0])
         }
     }, [query.data])
 
     if (!data) return <div className="flex items-center justify-center h-screen">Loading...</div>
 
-    return <div className="max-w-screen-lg h-full flex flex-col p-4">
-        <div className="min-h-[50px]">
+    return <div className="max-w-screen-lg h-full p-4 flex flex-col overflow-x-auto overflow-y-hidden">
+        <div>
             {/* pick role dropdown */}
-            <select className="p-2 border border-gray-300 rounded" defaultValue={selectedRole} onChange={e => setSelectedRole(e.target.value)}>
+            <select className="p-2 border border-gray-300 rounded" defaultValue={roleID} onChange={e => setRoleID(e.target.value)}>
                 {data?.getAllRoles?.map(role => <option key={role._id} value={role._id}>{role.data.name}</option>)}
             </select>
 
             {/* search */}
             <input type="text" className="p-2 border border-gray-300 rounded ml-2" placeholder="Search" />
-
+            
+            {/* update */}
             <button className="p-2.5 bg-primary text-white rounded ml-2">Update</button>
-
-            {/* scope editor */}
-            <ScopeEditor data={data} />
         </div>
-        <div className="h-[calc(100%-50px)] overflow-auto">
-            {/* role details */}
-            <ScopeTable selectedRole={selectedRole} data={data} />
+        <div className='max-h-1/2 overflow-auto'>
+            <ScopeEditor roleID={roleID} data={data} scope={scope} />
+        </div>
+        <div className='overflow-auto'>
+            <ScopeTable roleID={roleID} data={data} setScope={setScope} />
         </div>
     </div>
 }
 
-export function ScopeEditor({ data }: {data: client.GetRoleManagerDataQuery}) {
-    const [selectedScope, setSelectedScope] = React.useState<string>("core.Auth.use")
-    //const test: ValuesType<NonNullable<client.GetRoleManagerDataQuery["getAllAccessConfigs"]>>
-    if (!selectedScope) return <div></div>
-    
-    return <div className='pt-2 flex items-center'>
-        <select className="p-2 border border-gray-300 rounded" defaultValue={selectedScope} onChange={e => setSelectedScope(e.target.value)}>
-            {data.getAllAccessConfigs.map((access, index) => <option key={index} value={access.pack}>{getAlias(access.pack, data)}</option>)}
-        </select>
-        <button className="p-2.5 bg-primary text-white rounded ml-2">Add</button>
-    </div>
-}
-
-export function ScopeTable({ selectedRole, data }: {
-    selectedRole: string | undefined,
-    data: client.GetRoleManagerDataQuery 
+export function ScopeTable({ roleID, data, setScope }: {
+    roleID: string | undefined,
+    data: client.GetRoleManagerDataQuery,
+    setScope: (scope: Scope) => void
 }) {
-    if(!selectedRole) return <div></div>
-    const role = getRole(selectedRole, data)
+    if(!roleID) return <div></div>
+    const role = getRole(roleID, data)
     if(!role) return <div></div>
     const validator = new AccessValidator(role.data.resource_access)
 
     return <table className="w-full h-full table-auto border-r border-l">
-        <thead className="mt-4 bg-gray-200 sticky top-0">
+        <thead className="bg-gray-200 sticky top-0">
             <tr>
                 <th className="py-2 border-r border-gray-300">Granted</th>
                 <th className="py-2 border-r border-gray-300">Package</th>
@@ -91,7 +79,12 @@ export function ScopeTable({ selectedRole, data }: {
         </thead>
         <tbody>
             {data.getAllAccessConfigs.map((action, index) => 
-                <tr key={index} className="border-b border-gray-300">
+                <tr 
+                    onClick={() => setScope(action)}
+                    key={index} 
+                    className="border-b border-gray-300 hover:bg-gray-100 cursor-pointer"
+                    title={action?.info?.join("\n") || "-"}
+                >
                     <td className="p-1 border-r border-gray-300 text-nowrap text-center">
                         {validator.checkAccess(action) ? "✅" : "❌"}
                     </td>
@@ -111,4 +104,32 @@ export function ScopeTable({ selectedRole, data }: {
             )}
         </tbody>
     </table>
+}
+
+export function ScopeEditor({ roleID, data, scope }: {
+    roleID: string | undefined,
+    data: client.GetRoleManagerDataQuery,
+    scope: Scope | undefined
+}) {    
+    const role = getRole(roleID, data)
+    if(!scope || !role) return <div className="py-2"></div>
+    
+    const validator = new AccessValidator(role.data.resource_access)
+    const access = validator.findAccessProvided(scope)
+    
+    return <div className='h-full py-2 flex flex-col'>
+        <div className='text-lg font-semibold'>
+            Scope: {getAlias(scope.pack, data)} - {scope.resource} - {scope.action}
+        </div>
+        <div className='ml-2'>
+            Package:
+            <ul>
+                {access?.map((action, index) => 
+                    <li key={index}>
+                        {getAlias(action.pack, data)}.{action?.resource || "*"}.{action?.action || "*"}
+                    </li>
+                )}
+            </ul>
+        </div>
+    </div>
 }
